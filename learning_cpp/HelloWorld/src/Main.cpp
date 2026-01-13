e0946deb9cb3aa12007cfb81fcdd9fac54b5ddba
yusuf <yusuf.a.mail00@gmail.com>
2026-01-13 11:20:23 +0530

    pointer

--- learning_cpp/HelloWorld/src/Main.cpp
+++ learning_cpp/HelloWorld/src/Main.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-#include "Log.h"
+// #include "Log.h"
 #include<string>
 using namespace std ; 
 void log(const char*);
@@ -8,27 +8,36 @@ void log(const char*);
 //     log("multiply");
 //     return a * b; 
 // }
-void multiplyAndlog(int a , int b){
-  cout<<multiply(a,b)<<endl;
-}
-// for(init;condition;operation after going through the body)
+// void multiplyAndlog(int a , int b){
+//   cout<<multiply(a,b)<<endl;
+// }
+// // for(init;condition;operation after going through the body)
+// int main(){
+    
+//   for (int i = 0; i < 5; i++){
+//       if(i%2==0){
+//         continue; // skip this step and go back to beginning
+//       }
+//       log("hello world!");
+//       cout<<i<<endl;
+//     }
+//   int j = 0 ;
+//   while(j<10){
+//     if((j+1)%5==0){
+//       break; // exit the loop 
+//     }
+//     cout<<"iteration number : "<<j+1<<endl;
+//     j++;
+//   }
+    
+//     return 0 ; // not necessarily inside the loop 
+
+
 int main(){
-    
-  for (int i = 0; i < 5; i++){
-      if(i%2==0){
-        continue; // skip this step and go back to beginning
-      }
-      log("hello world!");
-      cout<<i<<endl;
-    }
-  int j = 0 ;
-  while(j<10){
-    if((j+1)%5==0){
-      break; // exit the loop 
-    }
-    cout<<"iteration number : "<<j+1<<endl;
-    j++;
-  }
-    
-    return 0 ; // not necessarily inside the loop f
+  int var = 8 ;
+  int* ptr = &var ; 
+  // pointer is holding address -> 0 is not a valid memory address or null
+  *ptr = 10 ; // can access and write data 
+  cout<<var<<endl;
+
 }
